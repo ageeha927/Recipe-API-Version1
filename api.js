6de21bc4ec73d84325309bde639ace705d5ac16e
hey.its.a <141285328+ageeha927@users.noreply.github.com>
2024-10-01 15:13:45 -0700

    .

--- api.js
+++ api.js
@@ -4,3 +4,18 @@ const fs = require('fs')
 const app = express()
 const PORT = 5000
 const path = require('path')
+
+// set the view to engine ejs
+app.set('view engine', 'ejs')
+
+app.use('/public', express.static('public'))
+// use res.render to load up an ejs view file
+
+//index page
+app.get('/', function(req, res) {
+    res.render('pages/index')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`)
+})
